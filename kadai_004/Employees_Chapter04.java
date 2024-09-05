e8645697232f111912a1c19c20b7270062aad7dd
Tororogohan1 <ri1003sen@docomo.ne.jp>
2024-09-05 19:12:18 +0900

    kadai_004ディレクトリを追加

---
+++ kadai_004/Employees_Chapter04.java
@@ -0,0 +1,59 @@
+package kadai_004;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Employees_Chapter04 {
+	
+  public static void main(String[] args) {
+	  
+	  Connection con = null;
+      Statement statement = null;
+      
+    try {
+    	
+    	 //データベース接続
+    	  con = DriverManager.getConnection(
+    		  "jdbc:mysql://localhost:3306/challenge_java",
+    		  "root",
+    		  "Rino0221"
+    	);
+    	  System.out.println("データベース接続成功");
+    	  
+    	  //SQLクエリを準備
+    	  statement = con.createStatement();
+    	  
+    	  //既存テーブル削除
+    	  String sqlDrop = "DROP TABLE IF EXISTS users";
+    	  statement.executeUpdate(sqlDrop);
+    	  
+    	  //テーブル作成
+    	  String sqlCreate = """
+    	  		       CREATE TABLE users (
+    	  		        id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    	  		        name VARCHAR(60) NOT NULL,
+    	  		        email VARCHAR(255) NOT NULL,
+    	  		        age INT(11),
+    	  		        address VARCHAR(255)
+    	  		       );
+    	  		       """;
+    
+    	  
+    	//　SQLクエリを実行（DBMSに送信）
+          int rowCnt = statement.executeUpdate(sqlCreate);
+          System.out.println("社員テーブルを作成しました:更新レコード数=" + rowCnt );
+      } catch(SQLException e) {
+          System.out.println("エラー発生：" + e.getMessage());
+      } finally {
+          // 使用したオブジェクトを解放
+          if( statement != null ) {
+              try { statement.close(); } catch(SQLException ignore) {}
+          }
+          if( con != null ) {
+              try { con.close(); } catch(SQLException ignore) {}
+          }
+      }
+  }
+}
